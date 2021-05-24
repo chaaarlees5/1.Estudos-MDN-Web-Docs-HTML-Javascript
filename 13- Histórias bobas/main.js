be2aed70ba4a02a827906b9332622d4171d62b90
Charlô <charlesoliveira0293@hotmail.com>
2021-05-24 20:25:00 -0300

    Alguns ajustes de sintaxe e comentários adicionados

--- 13- Histórias bobas/main.js
+++ 13- Histórias bobas/main.js
@@ -8,11 +8,11 @@ function randomValueFromArray(array){
 	const random = Math.floor(Math.random()*array.length);
 	return array[random];
 }
-
-var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+//História e complementos.
+var storyText = "Estava 34°C lá fora, então :insertx: foi dar um passeio. Quando eles chegaram na :inserty:, eles olharam horrorizados por alguns momentos, depois :insertz:. Bob viu tudo, mas não se surpreendeu - :insertx: pesa 135 quilos e foi um dia quente.";
 var insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-var insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-var insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+var insertY = ['cozinha', 'Disneylandia', 'Casa Branca'];
+var insertZ = ['combustão espontânea', 'derreteu em uma poça na calçada', 'se transformou em uma lesma e rastejou para longe'];
 
 randomize.addEventListener('click', result);
 
@@ -23,21 +23,23 @@ function result() {
 	var yItem = randomValueFromArray(insertY);
 	var zItem = randomValueFromArray(insertZ);
 	
-	newHistory = newHistory.replace(':insertx', xItem);
-	newHistory = newHistory.replace(':insertx', xItem);
-	newHistory = newHistory.replace(':inserty', yItem);
-	newHistory = newHistory.replace(':insertz', zItem);
+	newHistory = newHistory.replace(':insertx:', xItem);
+	newHistory = newHistory.replace(':insertx:', xItem);
+	newHistory = newHistory.replace(':inserty:', yItem);
+	newHistory = newHistory.replace(':insertz:', zItem);
 	
 	if(customName.value !== '') {
 		let name = customName.value;
 		newHistory = newHistory.replace('Bob', name);
+	} else {
+		window.alert('Você não digitou um nome, portanto o nome padrão será Bob')
 	}
 
 	if(document.getElementById("uk").checked) {
 		let weight = Math.round(300/14) + " stone";
 		let temperature =  Math.round((94-32) * (5/9)) + " centigrade";
-		newHistory = newHistory.replace('300 pounds', weight);
-		newHistory = newHistory.replace('94 fahrenheit', temperature);
+		newHistory = newHistory.replace('135 quilos', weight);
+		newHistory = newHistory.replace('34°C', temperature);
 	
 	}
 
